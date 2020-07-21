a61835c216402bab3fd5844723486e7e2d66d702
srinidi21 <shivanibuelah1606@gmail.com>
2020-07-21 15:18:23 +0530

    updated

--- src/org/test/BrowserLaunch.java
+++ src/org/test/BrowserLaunch.java
@@ -2,22 +2,30 @@ package org.test;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class BrowserLaunch {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\eclipse-workspace\\Selenium\\Chrome\\chromedriver_win32");
 		
 		WebDriver driver=new ChromeDriver();
 		
-		driver.get("https://www.facebook.com/");
+		driver.get("https://purple.com/");
 		
-		String URL=driver.getCurrentUrl();
-		System.out.println(URL);
+		WebElement bed = driver.findElement(By.xpath("(//a[@class='component-btn green m-b-4'])[2]"));
+		bed.click();
+		Thread.sleep(3000);
 		
-		String title=driver.getTitle();
-		System.out.println(title);
+		WebElement bed1 = driver.findElement(By.xpath("(//a[@class='component-btn green'])[3]"));
+		bed1.click();
+		Thread.sleep(3000);
+		
+		WebElement bed2 = driver.findElement(By.xpath("//span[text()='Add 1 Item To Cart']"));
+		bed2.click();
+		Thread.sleep(3000);
+		
 		
 		
 		
